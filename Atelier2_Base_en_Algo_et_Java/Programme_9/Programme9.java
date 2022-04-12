eaf8daf096f4a4b1cdd9ed427628fe9a8082d2fd
papi <papaibrahima98@gmail.com>
2022-04-12 16:02:04 +0000

    Saisie et affichage

--- Atelier2_Base_en_Algo_et_Java/Programme_9/Programme9.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_9/Programme9.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Programme9 {
-    
+    public static void main(String[] args) {
+        // Saisie et affichage
+        Scanner sc = new Scanner(System.in);
+        double reel;
+        int entier;
+        String caractere="";
+
+        System.out.println("Saisir le réel");
+        reel = sc.nextDouble();
+
+        System.out.println("Saisir l'entier");
+        entier = sc.nextInt();
+
+        System.out.println("Saisir la chaine de caratères");
+        caractere = sc.next();
+
+        System.out.println("Le réel saisi est:"+reel+".\nL'entier saisi est : "+entier+".\nLa chaine de caractères sais est :"+caractere);
+    }
 }
