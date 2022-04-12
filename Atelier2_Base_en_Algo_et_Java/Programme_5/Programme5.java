f59f192474daf9b3ef7baffb5fbd4ed959b1be57
papi <papaibrahima98@gmail.com>
2022-04-12 13:40:39 +0000

    quotient de deux nombres avec contrôle de nulabilite

--- Atelier2_Base_en_Algo_et_Java/Programme_5/Programme5.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_5/Programme5.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class Programme5 {
-    
+    public static void main(String[] args) {
+        // quotient de deux nombres entiers avec controle de nullabilité
+        Scanner sc = new Scanner(System.in);
+        int dividende = 0;
+        int diviseur = 1;
+        int quotient = 0;
+        System.out.println("Saisir la valeur du dividende");
+        dividende = sc.nextInt();
+        do {
+            System.out.println("Saisir la valeur du diviseur.Il doit être different de 0");
+            diviseur = sc.nextInt();
+        } while (diviseur==0);
+        quotient = dividende / diviseur;
+        System.out.println("Le quotient de "+dividende+" et de "+diviseur+"fait: "+quotient);
+    }   
 }
