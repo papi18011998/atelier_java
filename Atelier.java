1561e8553b02a9eba4d63499287c3fad7aa5f82e
papi <papaibrahima98@gmail.com>
2022-04-12 12:43:10 +0000

    nommenclature dossiers

--- Atelier.java
+++ Atelier.java
@@ -2,11 +2,6 @@ import java.util.Scanner;
 
 public class Atelier{
     public static void main(String[] args){
-        String nom = "";
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Entrez votre nom : ");
-        nom = sc.nextLine();
-        System.out.println("Bonjour " + nom + " !");
-        sc.close();
+        System.out.println("Mon premier code en Java");
     }
 }
