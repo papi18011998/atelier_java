5a5a5a261f0cf34e0f8784944eee308465524318
papi <papaibrahima98@gmail.com>
2022-04-12 13:20:35 +0000

    difference entre deux nombres

--- Atelier2_Base_en_Algo_et_Java/Programme_3/Programme3.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_3/Programme3.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Programme3 {
-    
+    public static void main(String[] args) {
+        // difference de deux nombres
+        Scanner sc = new Scanner(System.in);
+        int nombre_1 = 0;
+        int nombre_2 = 0;
+        int diffrence = 0;
+        System.out.println("Saisir le premier nombre");
+        nombre_1 = sc.nextInt();
+        System.out.println("Saisir le second nombre");
+        nombre_2 = sc.nextInt();
+        diffrence = nombre_1 - nombre_2;
+        System.out.println("La difference entre "+nombre_1+" et "+nombre_2+"fait: "+diffrence);
+    }
 }
