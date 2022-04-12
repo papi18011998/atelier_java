20f5b8555073082f4e6174cbb9a27096c2a2db13
papi <papaibrahima98@gmail.com>
2022-04-12 15:10:19 +0000

    positif negatif

--- Atelier2_Base_en_Algo_et_Java/Programme_7/Programme7.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_7/Programme7.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Programme7 {
-    
+    public static void main(String[] args) {
+        //  positif ou negatif
+        Scanner sc = new Scanner(System.in);
+        int nombre =0;
+        System.out.println("Saisir le nombre à tester !!!");
+        nombre = sc.nextInt();
+        if (nombre<0) {
+            System.out.println("Le nombre est négatif");
+        }else if (nombre>0) {
+            System.out.println("Le nombre est positif");
+        }else{
+            System.out.println("Le nombre est nul");
+        }
+    }
 }
