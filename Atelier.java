3e7a0e08d9e802c979f73ea5e2c47fb2b8c6c64e
papi <papaibrahima98@gmail.com>
2022-04-12 10:10:56 +0000

    atelier1: compilation java

---
+++ Atelier.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Atelier{
+    public static void main(String[] args){
+        String nom = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez votre nom : ");
+        nom = sc.nextLine();
+        System.out.println("Bonjour " + nom + " !");
+        
+    }
+}
