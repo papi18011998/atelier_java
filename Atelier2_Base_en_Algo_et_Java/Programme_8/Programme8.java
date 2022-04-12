fde47d97589eadf9a8c5837a0410643e7ca0f28d
papi <papaibrahima98@gmail.com>
2022-04-12 15:43:06 +0000

    affichage de 1 au nombre

--- Atelier2_Base_en_Algo_et_Java/Programme_8/Programme8.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_8/Programme8.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class Programme8 {
-    
+    public static void main(String[] args) {
+        // affichage de 1 au nombre
+        Scanner sc = new Scanner(System.in);
+        int nombre = 0;
+        System.out.println("Saisir un nombre");
+        nombre = sc.nextInt();
+        if (nombre>0) {
+            for(int i = 1; i<=nombre;i++){
+                System.out.println(i);
+            }
+        }else{
+            for(int i = nombre; i<=1;i++){
+                System.out.println(i);
+            }
+        }
+
+    }
 }
