26dab165f519f7b9b0e2493b8295d871b725d9bd
papi <papaibrahima98@gmail.com>
2022-04-12 13:14:08 +0000

    multiplcation de deux nombres

--- Atelier2_Base_en_Algo_et_Java/Programme_2/Programme2.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_2/Programme2.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
 public class Programme2 {
+    public static void main(String[] args) {
+        // produit de deux nombres
+        Scanner sc = new Scanner(System.in);
+        int nombre_1 = 0;
+        int nombre_2 = 0;
+        int produit = 0;
+        System.out.println("Saisir le premier nombre");
+        nombre_1 = sc.nextInt();
+        System.out.println("Saisir le second nombre");
+        nombre_2 = sc.nextInt();
+        produit = nombre_1 * nombre_2;
+        System.out.println("Le produit de "+nombre_1+" et de "+nombre_2+"fait: "+produit);
 
+    }
 }
