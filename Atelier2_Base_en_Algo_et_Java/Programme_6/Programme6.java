28e61473203bf38734a6fffd5be41fa244773d4a
papi <papaibrahima98@gmail.com>
2022-04-12 14:44:17 +0000

    moyenne

--- Atelier2_Base_en_Algo_et_Java/Programme_6/Programme6.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_6/Programme6.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Programme6 {
-    
+    public static void main(String[] args) {
+        // moyenne de 5 nombres ecrit au clavier
+        Scanner sc = new Scanner(System.in); 
+        int [] nombres = new int[5];
+        int i = 0;
+        int somme =0;
+        for(i=0;i<nombres.length;i++){
+            System.out.println("Saisir le nombre "+ (i+1));
+            nombres[i] = sc.nextInt();
+            somme = somme + nombres[i];
+        }
+        int moyennne = somme / nombres.length;
+        System.out.println("La moyenne des nombres fait: "+moyennne);
+    }    
 }
