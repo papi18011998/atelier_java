1a7431fefb7b881831cbafd1ba2814c5c8fe0f10
papi <papaibrahima98@gmail.com>
2022-04-12 13:32:23 +0000

    quotient de deux nombres

--- Atelier2_Base_en_Algo_et_Java/Programme_4/Programme4.java
+++ Atelier2_Base_en_Algo_et_Java/Programme_4/Programme4.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Programme4 {
-    
+    public static void main(String[] args) {
+        // quotient de deux nombres entiers
+        Scanner sc = new Scanner(System.in);
+        int dividende = 0;
+        int diviseur = 1;
+        int quotient = 0;
+        System.out.println("Saisir la valeur du dividende");
+        dividende = sc.nextInt();
+        do {
+            System.out.println("Saisir la valeur du diviseur.Il doit être different de 0");
+            diviseur = sc.nextInt();
+        } while (diviseur==0);
+        quotient = dividende / diviseur;
+        System.out.println("Le quotient de "+dividende+" et de "+diviseur+" fait: "+quotient);
+    }
 }
