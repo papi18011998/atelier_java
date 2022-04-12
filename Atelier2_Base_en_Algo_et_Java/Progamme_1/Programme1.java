bf7914f47b6a69d05472ccec09f0a91b263fc8d1
papi <papaibrahima98@gmail.com>
2022-04-12 13:07:02 +0000

    addition de deux nombres

--- Atelier2_Base_en_Algo_et_Java/Progamme_1/Programme1.java
+++ Atelier2_Base_en_Algo_et_Java/Progamme_1/Programme1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Programme1 {
     public static void main(String[] args) {
-        
+        // Somme de deux entiers saisis au clavier
+        Scanner sc = new Scanner(System.in);
+        int nombre_1 = 0;
+        int nombre_2 = 0;
+        int somme =0;
+        System.out.println("Saisir le premier nombre");
+        nombre_1 = sc.nextInt();
+        System.out.println("Saisir le second nombre");
+        nombre_2 = sc.nextInt();
+        somme = nombre_1 + nombre_2;
+        System.out.println("La somme de "+nombre_1+" et de "+nombre_2+"fait: "+somme);
     }
 }
