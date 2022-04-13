9120c7fdb564c8b980127468eafd6bfb2ddd618a
papi <papaibrahima98@gmail.com>
2022-04-13 13:14:33 +0000

    atelier 3: exo 2

---
+++ Atelier_3_sous_programmes/BoucleFor/BoucleFor.java
@@ -0,0 +1,8 @@
+public class BoucleFor {
+   public static void main(String[] args) {
+    //    affichage de 1 2 3 4 5 dans l'ordre
+    for(int i=1;i<=5;i++){
+        System.out.println(i);
+    }
+   } 
+}
