dbfc8799e512f8ccdd0fbf7e6e144ed657a8b346
papi <papaibrahima98@gmail.com>
2022-04-13 13:20:23 +0000

    atelier 3: exo 4

---
+++ Atelier_3_sous_programmes/BoucleDoWhile/BoucleDoWhile.java
@@ -0,0 +1,10 @@
+public class BoucleDoWhile {
+    public static void main(String[] args) {
+        // affichage de 1 2 3 4 5
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<=5);
+    }
+}
